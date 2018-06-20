6e91552511d7ed7ef683ebdc83f6f5ac63fa4be1
houjianping <houjianping@iyuedan.com>
2018-06-20 14:04:09 +0800

    code style

--- base_library/src/main/java/com/androidapp/flowlayout/TagAdapter.java
+++ base_library/src/main/java/com/androidapp/flowlayout/TagAdapter.java
@@ -8,68 +8,59 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-public abstract class TagAdapter<T>{
+public abstract class TagAdapter<T> {
     private List<T> mTagDatas;
     private OnDataChangedListener mOnDataChangedListener;
     private HashSet<Integer> mCheckedPosList = new HashSet<Integer>();
 
-    public TagAdapter(List<T> datas){
+    public TagAdapter(List<T> datas) {
         mTagDatas = datas;
     }
 
-    public TagAdapter(T[] datas){
+    public TagAdapter(T[] datas) {
         mTagDatas = new ArrayList<T>(Arrays.asList(datas));
     }
 
-    static interface OnDataChangedListener
-    {
+    static interface OnDataChangedListener {
         void onChanged();
     }
 
-    void setOnDataChangedListener(OnDataChangedListener listener)
-    {
+    void setOnDataChangedListener(OnDataChangedListener listener) {
         mOnDataChangedListener = listener;
     }
 
-    public void setSelectedList(int... pos)
-    {
+    public void setSelectedList(int... pos) {
         for (int i = 0; i < pos.length; i++)
             mCheckedPosList.add(pos[i]);
         notifyDataChanged();
     }
 
-    public void setSelectedList(Set<Integer> set)
-    {
+    public void setSelectedList(Set<Integer> set) {
         mCheckedPosList.clear();
         mCheckedPosList.addAll(set);
         notifyDataChanged();
     }
 
-    HashSet<Integer> getPreCheckedList()
-    {
+    HashSet<Integer> getPreCheckedList() {
         return mCheckedPosList;
     }
 
 
-    public int getCount()
-    {
+    public int getCount() {
         return mTagDatas == null ? 0 : mTagDatas.size();
     }
 
-    public void notifyDataChanged()
-    {
+    public void notifyDataChanged() {
         mOnDataChangedListener.onChanged();
     }
 
-    public T getItem(int position)
-    {
+    public T getItem(int position) {
         return mTagDatas.get(position);
     }
 
     public abstract View getView(FlowLayout parent, int position, T t);
 
-    public boolean setSelected(int position ,T t)
-    {
+    public boolean setSelected(int position, T t) {
         return false;
     }
 
